8efb4dcb39568e01107ef250d47781b7de8d6623
Thunder <gilbertnepomuceno99@gmail.com>
2025-02-22 20:59:25 -0800

    basic intials

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 
-#PlaceHolder
+import pygame, sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((500,500))
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
